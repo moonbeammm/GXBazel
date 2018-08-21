32c4e161667620cb5c3c4d5482fcbd016d286010
sunguangxin <sunguangxin@bilibili.com>
2018-08-22 01:35:35 +0800

    使用bazel初始化工程

---
+++ universal/universal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by sunguangxin on 2018/8/18.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ universal/universal/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by sunguangxin on 2018/8/18.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
