ceab9a620c9bc6f4d000fa93651927a20f423ef0
sgx <sunguangxin@bilibili.com>
2020-07-22 16:33:59 +0800

    --user=sunguangxin --task=添加GXPlayer库。

---
+++ srcs/app/GXPlayer/GXPlayer/GXPlayer/GXPlayerViewController.m
@@ -0,0 +1,24 @@
+//
+//  GXPlayerViewController.m
+//  Test
+//
+//  Created by sunguangxin on 2018/8/18.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+#import "GXPlayerViewController.h"
+
+
+@interface GXPlayerViewController ()
+
+@end
+
+@implementation GXPlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
---
+++ srcs/app/GXPlayer/include/GXPlayer/GXPlayerViewController.h
@@ -0,0 +1,15 @@
+//
+//  GXPlayerViewController.h
+//  Test
+//
+//  Created by sunguangxin on 2018/8/18.
+//  Copyright © 2018年 sunguangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXPlayerViewController : UIViewController
+
+
+@end
+
